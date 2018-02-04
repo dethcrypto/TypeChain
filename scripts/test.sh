68fc21d371967ec278da6fc54af9c7c774e16809
Krzysztof Kaczor <chris@kaczor.io>
2018-02-04 20:24:47 +0100

    add support for outDir option

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,12 +8,18 @@ yarn test:unit
 echo "Praparing for integration tests"
 
 ABI_DIR="./test/integration/abis"
+ABI_TMP_DIR="./test-tmp"
 rm -rf $ABI_DIR
+rm -rf $ABI_TMP_DIR
+
+mkdir $ABI_TMP_DIR 
 
 echo "Generating ABIs for sample contracts"
 solc --abi ./test/integration/contracts/* --bin -o $ABI_DIR
 
 echo "Generate Typechain wrappers"
 yarn test:generateContracts
+echo "test --outdir..."
+yarn test:generateContractsOutDir
 
 yarn test:integration
