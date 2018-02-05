9349d61ee75a847af38a78e8e9f593e647b3747e
Linus Norton <linusnorton@gmail.com>
2018-02-05 20:22:40 +0000

    now return strings instead of bignumber

--- test/integration/ContractWithOverloads.spec.ts
+++ test/integration/ContractWithOverloads.spec.ts
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 import { deployContract } from "./utils/web3Contracts";
-
 import { web3 } from "./web3";
 import { __ContractWithOverloads_sol_ContractWithOverloads as ContractWithOverloads } from "./abis/__ContractWithOverloads_sol_ContractWithOverloads";
+import { BigNumber } from "bignumber.js";
 
 describe("ContractWithOverloads", () => {
   let contractAddress: string;
@@ -16,7 +16,7 @@ describe("ContractWithOverloads", () => {
       web3,
       contractAddress
     );
-    const result = await contractWithOverloads.counter;
+    const result = new BigNumber(await contractWithOverloads.counter);
 
     // numbers now returned as string
     expect(result.toNumber()).to.be.eq(0);
