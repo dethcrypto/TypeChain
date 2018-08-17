0710c861c3cc2c3ef8548d83611c812f9ddd79f6
Krzysztof Kaczor <chris@kaczor.io>
2018-08-17 15:50:23 +0200

    simplify test script, move all code generation operations to mocha env

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,7 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
-yarn test:unit
-
-echo "Praparing for integration tests"
+echo "Cleanup"
 
 cd ./test/integration/contracts/
 ABI_DIR="../abis"
@@ -18,12 +16,7 @@ mkdir $ABI_TMP_DIR
 echo "Generating ABIs for sample contracts"
 ../../../node_modules/.bin/solcjs --abi ./* --bin -o $ABI_DIR
 
-echo "Generate TypeChain wrappers"
-yarn test:generateContracts
-echo "test --outdir..."
-yarn test:generateContractsOutDir
+yarn test:mocha
 
 echo "Testing if there are no complication problems with generated wrappers"
 yarn tsc
-
-yarn test:integration
