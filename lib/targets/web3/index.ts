0581946871ecd7e20fffbefb40e69f3b3d63b871
James Lefrère <james@colony.io>
2019-05-04 16:17:31 +0200

    Improve event types (web3-1.0.0)
    * Parse event inputs in order to improve the typing of contract events for the `web3-1.0.0` target
    * Update existing types to reflect updated `web3` (e.g. `EstimateGasOptions`)

--- lib/targets/web3/index.ts
+++ lib/targets/web3/index.ts
@@ -1,4 +1,3 @@
-import { Contract } from "../../parser/abiParser";
 import { TsGeneratorPlugin, TContext, TFileDesc } from "ts-generator";
 import { join } from "path";
 import { extractAbi, parse } from "../../parser/abiParser";
@@ -46,17 +45,31 @@ export class Web3 extends TsGeneratorPlugin {
       {
         path: join(this.outDirAbs, "types.d.ts"),
         contents: `
-  import { Transaction } from "web3-core";
+  import { EventLog } from "web3-core";
+  import BN from "bn.js";
+  import { EstimateGasOptions, EventOptions } from "web3-eth-contract";
+  import { EventEmitter } from "events";
   // @ts-ignore
   import PromiEvent from "web3-core-promievent";
   export type Callback<T> = (error: Error, result: T) => void;
   export interface TransactionObject<T> {
     arguments: any[];
-    call(tx?: Transaction): Promise<T>;
-    send(tx?: Transaction): PromiEvent<T>;
-    estimateGas(tx?: Transaction): Promise<number>;
+    call(options?: EstimateGasOptions): Promise<T>;
+    send(options?: EstimateGasOptions): PromiEvent<T>;
+    estimateGas(options?: EstimateGasOptions): Promise<number>;
     encodeABI(): string;
-  }`,
+  }
+  export interface ContractEventLog<T> extends EventLog {
+    returnValues: T;
+  }
+  export interface ContractEventEmitter<T> extends EventEmitter {
+    on(event: 'data' | 'changed', listener: (event: ContractEventLog<T>) => void): this;
+    on(event: 'error', listener: (error: Error) => void): this;
+  }
+  export type ContractEvent<T> = (
+    options?: EventOptions,
+    cb?: Callback<ContractEventLog<T>>
+  ) => ContractEventEmitter<T>;`,
       },
     ];
   }
