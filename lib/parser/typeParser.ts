874097ded8f4c8cba6b0b5f4f6f16c3cce100467
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-09-21 23:36:02 +0200

    rewrite type parser

--- lib/parser/typeParser.ts
+++ lib/parser/typeParser.ts
@@ -1,51 +1,41 @@
-/**
- * This will be refactored in the near future. Parser should produce discriminating union types.
- */
+// represent all possible EvmTypes using TypeScript's discriminating union
+export type EvmType =
+  | BooleanType
+  | IntegerType
+  | UnsignedIntegerType
+  | VoidType
+  | StringType
+  | BytesType
+  | DynamicBytesType
+  | AddressType
+  | ArrayType
+  | TupleType;
 
-export type EvmType = BooleanType | IntegerType | UnsignedIntegerType;
-
-export type BooleanType = {
-  type: "boolean";
-};
-
-export type IntegerType = {
-  type: "integer";
-  bits: number;
-};
-
-export type UnsignedIntegerType = {
-  type: "uinteger";
-  bits: number;
-};
-
+export type BooleanType = { type: "boolean" };
+export type IntegerType = { type: "integer"; bits: number };
+export type UnsignedIntegerType = { type: "uinteger"; bits: number };
 export type VoidType = { type: "void" };
 export type StringType = { type: "string" };
 export type BytesType = { type: "bytes"; size: number };
-
 export type DynamicBytesType = { type: "dynamic-bytes" };
 export type AddressType = { type: "address" };
-
 export type ArrayType = { type: "array"; itemType: EvmType; size?: number };
+export type TupleType = { type: "tuple"; components: EvmSymbol[] };
 
-export class EvmTypeComponent {
-  constructor(public readonly name: string, public readonly type: EvmType) {}
-}
-
-export class TupleType extends EvmType {
-  constructor(public readonly components: EvmTypeComponent[]) {
-    super();
-  }
-}
+export type EvmSymbol = {
+  type: EvmType;
+  name: string;
+};
 
 const isUIntTypeRegex = /^uint([0-9]*)$/;
 const isIntTypeRegex = /^int([0-9]*)$/;
 const isBytesTypeRegex = /^bytes([0-9]+)$/;
 
-export function parseEvmType(rawType: string, components?: EvmTypeComponent[]): EvmType {
+export function parseEvmType(rawType: string, components?: EvmSymbol[]): EvmType {
   const lastChar = rawType[rawType.length - 1];
 
+  // first we parse array type
   if (lastChar === "]") {
-    // we parse array type
     let finishArrayTypeIndex = rawType.length - 2;
     while (rawType[finishArrayTypeIndex] !== "[") {
       finishArrayTypeIndex--;
@@ -56,41 +46,41 @@ export function parseEvmType(rawType: string, components?: EvmTypeComponent[]): 
 
     const restOfTheType = rawType.slice(0, finishArrayTypeIndex);
 
-    return new ArrayType(parseEvmType(restOfTheType, components), arraySize);
+    return { type: "array", itemType: parseEvmType(restOfTheType, components), size: arraySize };
   }
 
-  // this has to be primitive type
+  // otherwise this has to be primitive type
 
-  //first deal with simple types
+  // deal with simple to parse types
   switch (rawType) {
     case "bool":
-      return new BooleanType();
+      return { type: "boolean" };
     case "address":
-      return new AddressType();
+      return { type: "address" };
     case "string":
-      return new StringType();
+      return { type: "string" };
     case "byte":
-      return new BytesType(1);
+      return { type: "bytes", size: 1 };
     case "bytes":
-      return new DynamicBytesType();
+      return { type: "dynamic-bytes" };
     case "tuple":
       if (!components) throw new Error("Tuple specified without components!");
-      return new TupleType(components);
+      return { type: "tuple", components };
   }
 
   if (isUIntTypeRegex.test(rawType)) {
     const match = isUIntTypeRegex.exec(rawType);
-    return new UnsignedIntegerType(parseInt(match![1] || "256"));
+    return { type: "uinteger", bits: parseInt(match![1] || "256") };
   }
 
   if (isIntTypeRegex.test(rawType)) {
     const match = isIntTypeRegex.exec(rawType);
-    return new IntegerType(parseInt(match![1] || "256"));
+    return { type: "integer", bits: parseInt(match![1] || "256") };
   }
 
   if (isBytesTypeRegex.test(rawType)) {
     const match = isBytesTypeRegex.exec(rawType);
-    return new BytesType(parseInt(match![1] || "1"));
+    return { type: "bytes", size: parseInt(match![1] || "1") };
   }
 
   throw new Error("Unknown type: " + rawType);
