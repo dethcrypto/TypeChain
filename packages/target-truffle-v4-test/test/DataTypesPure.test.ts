073fbb32381486282f74789e0bc8825fe1c482c8
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-04-11 22:37:32 +0200

    [target-truffle-v4] Add pure and view tests

---
+++ packages/target-truffle-v4-test/test/DataTypesPure.test.ts
@@ -0,0 +1,43 @@
+import { typedAssert, q18 } from 'test-utils'
+import { DataTypesPureInstance } from '../types/truffle-contracts'
+import BigNumber from 'bignumber.js'
+
+const DataTypesPure = artifacts.require('DataTypesPure')
+
+contract('DataTypesPure', ([deployer]) => {
+  let c: DataTypesPureInstance
+
+  beforeEach(async () => {
+    c = await DataTypesPure.new({ from: deployer })
+  })
+
+  it('works', async () => {
+    typedAssert(await c.pure_uint8(), new BigNumber('42'))
+    typedAssert(await c.pure_uint256(), new BigNumber(q18(1)))
+
+    typedAssert(await c.pure_int8(), new BigNumber('42'))
+    typedAssert(await c.pure_int256(), new BigNumber(q18(1)))
+
+    typedAssert(await c.pure_bool(), true)
+
+    typedAssert(await c.pure_address(), '0x70b144972c5ef6cb941a5379240b74239c418cd4')
+
+    typedAssert(await c.pure_bytes1(), '0xaa')
+
+    typedAssert(await c.pure_bytes(), '0x54797065436861696e')
+
+    typedAssert(await c.pure_string(), 'TypeChain')
+
+    typedAssert(await c.pure_stat_array(), [new BigNumber('1'), new BigNumber('2'), new BigNumber('3')])
+
+    typedAssert(await c.pure_tuple(), [new BigNumber('1'), new BigNumber('2')])
+
+    // structs doesnt work: could be because of solidty 0.4.x but we are stuck at it
+    // typedAssert(await c.pure_struct(), {
+    //   uint256_0: new BigNumber('1'),
+    //   uint256_1: new BigNumber('2'),
+    // })
+
+    typedAssert(await c.pure_enum(), new BigNumber('1'))
+  })
+})
