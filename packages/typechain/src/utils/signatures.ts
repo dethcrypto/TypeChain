763788e75237b0f80aff61948ec52246e9019ea1
NickLatkovich <nicklatkovich@gmail.com>
2021-01-25 18:07:24 +0300

    Fix naming

--- packages/typechain/src/utils/signatures.ts
+++ packages/typechain/src/utils/signatures.ts
@@ -16,9 +16,9 @@ export function getIndexedSignatureForEvent(event: EventDeclaration): string {
 
 export function getArgumentForSignature(argument: AbiParameter): string {
   if (argument.type.originalType !== 'tuple') return argument.type.originalType
-  return `(${(argument.type as TupleType).components.map((i) => getArgumentSignature(i)).join(',')})`
+  return `(${(argument.type as TupleType).components.map((i) => getArgumentForSignature(i)).join(',')})`
 }
 
 export function getSignatureForFn(fn: FunctionDeclaration): string {
-  return `${fn.name}(${fn.inputs.map((i) => getArgumentSignature(i)).join(',')})`
+  return `${fn.name}(${fn.inputs.map((i) => getArgumentForSignature(i)).join(',')})`
 }
