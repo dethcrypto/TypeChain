a7aca7780ec2b491c364afbc53bdd230e3e0348d
Soham Zemse <22412996+zemse@users.noreply.github.com>
2021-03-07 21:12:11 +0530

    [ethers-v5] Regenerate types

--- packages/target-ethers-v5-test/types/commons.ts
+++ packages/target-ethers-v5-test/types/commons.ts
@@ -5,7 +5,7 @@
 import { EventFilter, Event } from "ethers";
 import { Result } from "@ethersproject/abi";
 
-export interface TypedEventFilter<EventArgsArray, EventArgsObject>
+export interface TypedEventFilter<_EventArgsArray, _EventArgsObject>
   extends EventFilter {}
 
 export interface TypedEvent<EventArgs extends Result> extends Event {
