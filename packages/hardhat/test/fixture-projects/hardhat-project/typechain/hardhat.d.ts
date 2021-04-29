2f39f72c6dce5d765a76273302b9057a893855ab
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-04-30 00:01:06 +0200

    Update type generation for hardhat.d.ts

--- packages/hardhat/test/fixture-projects/hardhat-project/typechain/hardhat.d.ts
+++ packages/hardhat/test/fixture-projects/hardhat-project/typechain/hardhat.d.ts
@@ -3,21 +3,37 @@
 /* eslint-disable */
 
 import { ethers } from "ethers";
-import { FactoryOptions } from "@nomiclabs/hardhat-ethers/types";
+import {
+  FactoryOptions,
+  HardhatEthersHelpers as HardhatEthersHelpersBase,
+} from "@nomiclabs/hardhat-ethers/types";
 
 import * as Contracts from ".";
 
 declare module "hardhat/types/runtime" {
-  function getContractFactory(
-    name: "SafeMath",
-    signerOrOptions?: ethers.Signer | FactoryOptions
-  ): Promise<Contracts.SafeMath__factory>;
-  function getContractFactory(
-    name: "TestContract",
-    signerOrOptions?: ethers.Signer | FactoryOptions
-  ): Promise<Contracts.TestContract__factory>;
-  function getContractFactory(
-    name: "TestContract1",
-    signerOrOptions?: ethers.Signer | FactoryOptions
-  ): Promise<Contracts.TestContract1__factory>;
+  interface HardhatEthersHelpers extends HardhatEthersHelpersBase {
+    getContractFactory(
+      name: "SafeMath",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.SafeMath__factory>;
+    getContractFactory(
+      name: "TestContract",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.TestContract__factory>;
+    getContractFactory(
+      name: "TestContract1",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.TestContract1__factory>;
+
+    // default types
+    getContractFactory(
+      name: string,
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<ethers.ContractFactory>;
+    getContractFactory(
+      abi: any[],
+      bytecode: ethers.utils.BytesLike,
+      signer?: ethers.Signer
+    ): Promise<ethers.ContractFactory>;
+  }
 }
--- packages/target-ethers-v5/src/codegen/hardhat.ts
+++ packages/target-ethers-v5/src/codegen/hardhat.ts
@@ -5,19 +5,32 @@ export function generateHardhatHelper(contracts: string[]): string {
   return `
 
 import { ethers } from 'ethers'
-import { FactoryOptions } from '@nomiclabs/hardhat-ethers/types'
+import { FactoryOptions, HardhatEthersHelpers as  HardhatEthersHelpersBase} from "@nomiclabs/hardhat-ethers/types";
 
 import * as Contracts from "."
 
-declare module 'hardhat/types/runtime' {
+declare module "hardhat/types/runtime" {
+  interface HardhatEthersHelpers extends HardhatEthersHelpersBase {
   ${contracts
     .map(
       (n) =>
-        `function getContractFactory(name: '${n}', signerOrOptions?: ethers.Signer | FactoryOptions): Promise<Contracts.${
+        `getContractFactory(name: '${n}', signerOrOptions?: ethers.Signer | FactoryOptions): Promise<Contracts.${
           n + FACTORY_POSTFIX
         }>`,
     )
     .join('\n')}
+
+    // default types
+    getContractFactory(
+      name: string,
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<ethers.ContractFactory>;
+    getContractFactory(
+      abi: any[],
+      bytecode: ethers.utils.BytesLike,
+      signer?: ethers.Signer
+    ): Promise<ethers.ContractFactory>;
+  }
 }
   `
 }
