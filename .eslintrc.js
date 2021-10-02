5b7bc49f25309c2f171b567c8699c3e61e5c6fae
hasparus <hasparus@gmail.com>
2021-10-02 11:03:16 +0200

    Include *.js files in tsconfig.json for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   env: {
     es6: true,
@@ -11,7 +12,7 @@ module.exports = {
   },
   rules: {
     // this gets inlined into a package eslint, so it means: use current package's package.info or the one at the project root
-    'import/no-extraneous-dependencies': ['error', { packageDir: ['./', '../../'] }],
+    'import/no-extraneous-dependencies': ['error', { packageDir: ['./', __dirname] }],
     'unused-imports/no-unused-imports-ts': 'error',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
