4e202dd56c40acc8c9f69387aba88225212d9bd6
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-10-26 23:58:34 +0200

    Add failing tests for factory constructors

--- packages/hardhat-test/test/hardhat.test.ts
+++ packages/hardhat-test/test/hardhat.test.ts
@@ -1,3 +1,4 @@
+import { Interface } from '@ethersproject/abi'
 import { ethers } from 'hardhat'
 import { AssertTrue, IsExact } from 'test-utils'
 
@@ -8,5 +9,15 @@ describe('TypeChain x Hardhat', () => {
     const counterFactory = await ethers.getContractFactory('Counter')
 
     type _ = AssertTrue<IsExact<typeof counterFactory, Counter__factory>>
+  })
+
+  it('should construct factories in different ways', async () => {
+    const [deployer] = await ethers.getSigners()
+    const artifact = require('../artifacts/contracts/Counter.sol/Counter.json') as { abi: Interface; bytecode: string }
+
+    const counterFactory1 = new Counter__factory()
+    const counterFactory2 = new Counter__factory(deployer)
+    const counterFactory3 = new Counter__factory(artifact.abi, artifact.bytecode)
+    const counterFactory4 = new Counter__factory(artifact.abi, artifact.bytecode, deployer)
   })
 })
