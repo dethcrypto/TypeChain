d54f8e83fa5a37b12f28596d0a1829943eb09b0a
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-04-22 17:57:24 +0200

    Init

---
+++ packages/target-starknet-test/test/dai.test.ts
@@ -0,0 +1,5 @@
+describe('Dai', () => {
+  it('works', () => {
+    // this should deploy actual contact on local starknet instance and do runtime checks to ensure that types from typings, match actual types
+  })
+})
---
+++ packages/target-starknet/README.md
@@ -0,0 +1,1 @@
+# Typechain target Starknet
