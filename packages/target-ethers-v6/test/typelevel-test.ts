10263a358c8c9440beca8462f29f0e95432dc353
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-02-05 15:41:01 +0000

    Fix typecheck

--- packages/target-ethers-v6/test/typelevel-test.ts
+++ packages/target-ethers-v6/test/typelevel-test.ts
@@ -3,7 +3,11 @@ import { AssertTrue, IsExact } from 'test-utils'
 
 import { TypedContractEvent, TypedEventLog, TypedListener } from '../static/common'
 
-export type TransferEvent = TypedContractEvent<[string, string, number], { from: string; to: string; tokenId: number }>
+export type TransferEvent = TypedContractEvent<
+  [string, string, number],
+  [string, string, number],
+  { from: string; to: string; tokenId: number }
+>
 
 type _ = AssertTrue<
   IsExact<
