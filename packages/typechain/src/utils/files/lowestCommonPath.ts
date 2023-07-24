320fde767f202d469e686e05e7804d34c3151bd7
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-07-24 09:01:59 +0200

    Format fix

--- packages/typechain/src/utils/files/lowestCommonPath.ts
+++ packages/typechain/src/utils/files/lowestCommonPath.ts
@@ -1,13 +1,16 @@
 export function lowestCommonPath(paths: string[]) {
   const pathParts = paths.map((path) => path.split(/[\\/]/))
-  const commonParts = [] as string[];
-  const maxParts = Math.min.apply(null, pathParts.map(p => p.length));
+  const commonParts = [] as string[]
+  const maxParts = Math.min.apply(
+    null,
+    pathParts.map((p) => p.length),
+  )
   for (let i = 0; i < maxParts; i++) {
-    const part = pathParts[0][i];
+    const part = pathParts[0][i]
     if (pathParts.slice(1).every((otherPath) => otherPath[i] === part)) {
       commonParts.push(part)
     } else {
-      break;
+      break
     }
   }
   return commonParts.join('/')
